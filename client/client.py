befb0e350880da9177362a1082134bb04c56e02d
Oisin Cleary <oisin@Oisins-MacBook-Pro-2.local>
2016-12-15 14:45:35 +0000

    added simple file upload

--- client/client.py
+++ client/client.py
@@ -9,6 +9,15 @@ def evaluate(command):
         else:
             r = requests.post('http://localhost:5000/login', data={'username': args[1], 'password': args[2]})
             print r.text
+    elif args[0] == 'add':
+        if len(args) != 2:
+            print 'login takes 1 argument\nadd $filename'
+        else:
+            path = 'files/' + args[1]
+            f = open(path)
+            r = requests.post('http://localhost:5000/add', data={'filename': args[1], 'file': f.read()})
+            # r = requests.post('http://posttestserver.com/post.php', data={'filename': args[1], 'file': f.read()})
+            print r.text
     else:
         print 'invalid command ' + args[0]
 
--- fileServer/fileServer.py
+++ fileServer/fileServer.py
@@ -72,17 +72,17 @@ def close_db(error):
 def show_entries():
     db = get_db()
     cur = db.execute('select title, text from entries order by id desc')
-    entries = cur.fetchall()
-    return str(entries)
+    entryNames = cur.fetchone()
+    return entryNames["title"]
 
 
 @app.route('/add', methods=['POST'])
 def add_entry():
-    if not session.get('logged_in'):
-        abort(401)
+    # if not session.get('logged_in'):
+    #     abort(401)
     db = get_db()
     db.execute('insert into entries (title, text) values (?, ?)',
-               [request.form['title'], request.form['text']])
+               [request.form['filename'], request.form['file']])
     db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
