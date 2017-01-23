28eaa4fde70bb005a2fb8db2bcff978d982e47cf
Oisin Cleary <oisin@Oisins-MacBook-Pro-2.local>
2017-01-23 16:18:52 +0000

    reformatted client slighty

--- client/client.py
+++ client/client.py
@@ -4,53 +4,77 @@ directoryServerUrl = 'http://localhost:3000/'
 authServerUrl = 'http://localhost:4000/'
 
 
+def login(args):
+    if len(args) != 3:
+        print 'login takes 2 arguments\nlogin $username $pword'
+    else:
+        r = requests.post((authServerUrl + 'login'), data={'username': args[1], 'password': args[2]})
+        print r.text
+
+
+def addUser(args):
+    if len(args) != 4:
+        print 'addUser takes 3 arguments\naddUser $ level $username $pword'
+    else:
+        r = requests.post((authServerUrl + 'addUser'), data={'level': args[1], 'username': args[2],
+                          'password': args[3]})
+        print r.text
+
+
+def addFile(args):
+    if len(args) != 2:
+        print 'add takes 1 argument\nadd $filename'
+    else:
+        path = 'files/' + args[1]
+        f = open(path)
+        r = requests.post((directoryServerUrl + 'add'), data={'filename': args[1], 'file': f.read()})
+        print r.text
+
+
+def listFiles(args):
+    if len(args) != 1:
+        print 'list takes no arguments'
+    else:
+        r = requests.get(directoryServerUrl)
+        print r.text
+
+
+def listUsers(args):
+    if len(args) != 1:
+        print 'listUsers takes no arguments'
+    else:
+        r = requests.get(authServerUrl)
+        print r.text
+
+
+def getFile(args):
+    if len(args) != 2:
+        print 'get takes 1 argument\nget $filename'
+    else:
+        r = requests.post((directoryServerUrl + 'get'), data={'filename': args[1]})
+        if r.text == 'Invalid name':
+            print 'Invalid name'
+        else:
+            path = 'files/' + args[1]
+            f = open(path, 'w')
+            f.write(r.text)
+            print 'file downloaded'
+
+
 def evaluate(command):
     args = command.split(' ')
     if args[0] == 'login':
-        if len(args) != 3:
-            print 'login takes 2 arguments\nlogin $username $pword'
-        else:
-            r = requests.post((authServerUrl + 'login'), data={'username': args[1], 'password': args[2]})
-            print r.text
+        login(args)
     elif args[0] == 'addUser':
-        if len(args) != 4:
-            print 'addUser takes 3 arguments\naddUser $ level $username $pword'
-        else:
-            r = requests.post((authServerUrl + 'addUser'), data={'level': args[1], 'username': args[2],
-                              'password': args[3]})
-            print r.text
+        addUser(args)
     elif args[0] == 'add':
-        if len(args) != 2:
-            print 'add takes 1 argument\nadd $filename'
-        else:
-            path = 'files/' + args[1]
-            f = open(path)
-            r = requests.post((directoryServerUrl + 'add'), data={'filename': args[1], 'file': f.read()})
-            print r.text
+        addFile(args)
     elif args[0] == 'list':
-        if len(args) != 1:
-            print 'list takes no arguments'
-        else:
-            r = requests.get(directoryServerUrl)
-            print r.text
+        listFiles(args)
     elif args[0] == 'listUsers':
-        if len(args) != 1:
-            print 'listUsers takes no arguments'
-        else:
-            r = requests.get(authServerUrl)
-            print r.text
+        listUsers(args)
     elif args[0] == 'get':
-        if len(args) != 2:
-            print 'get takes 1 argument\nget $filename'
-        else:
-            r = requests.post((directoryServerUrl + 'get'), data={'filename': args[1]})
-            if r.text == 'Invalid name':
-                print 'Invalid name'
-            else:
-                path = 'files/' + args[1]
-                f = open(path, 'w')
-                f.write(r.text)
-                print 'file downloaded'
+        getFile(args)
     else:
         print 'invalid command ' + args[0]
 
--- fileServer/fileServer.py
+++ fileServer/fileServer.py
@@ -1,15 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-    Flaskr
-    ~~~~~~
-
-    A microblog example application written as Flask tutorial with
-    Flask and sqlite3.
-
-    :copyright: (c) 2015 by Armin Ronacher.
-    :license: BSD, see LICENSE for more details.
-"""
-
 import os
 from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
